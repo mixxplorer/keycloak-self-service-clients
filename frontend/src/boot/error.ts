57fe1d74ce6892415e91c0c9bb2e3299793895a7
Leonard Marschke <leonard@marschke.me>
2024-06-06 13:03:05 +0200

    refactor error handling to catch errors in try blocks

--- frontend/src/boot/error.ts
+++ frontend/src/boot/error.ts
@@ -1,42 +1,52 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
+import { AxiosError } from 'axios'
 import { boot } from 'quasar/wrappers'
 
 import { Notifier } from 'src/components/notifier/Notifier'
 
-export default boot((): void => {
+interface ErrorPayload {
+  error?: string;
+  errorMessage?: string;
+}
+
+export default boot(({ app }): void => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   window.onerror = function (msg, url, line, col, error) {
+    // eslint-disable-next-line no-console
+    console.log(msg)
+    // eslint-disable-next-line @typescript-eslint/no-base-to-string
     Notifier.showErrorMessage(`Uncaught exception: ${msg}`, true)
     return true
   }
 
-  window.onunhandledrejection = function (errorEvent) {
+  const appErrorHandler = function (generalError: Error | undefined) {
     if (
-      errorEvent.reason !== undefined &&
-      errorEvent.reason.name === 'AxiosError'
+      generalError !== undefined &&
+      generalError.name === 'AxiosError'
     ) {
+      const axiosError = generalError as AxiosError<ErrorPayload>
       if (
-        errorEvent.reason.response.data !== undefined &&
-        typeof errorEvent.reason.response.data.error === 'string'
+        axiosError.response?.data !== undefined &&
+        typeof axiosError.response?.data?.error === 'string'
       ) {
         Notifier.showErrorMessage(
-          `Network request failed with "${errorEvent.reason.response.data.error}"
-          (Status code ${errorEvent.reason.response.status}). For more information see dev console.`,
+          `Network request failed with "${axiosError.response?.data?.error}"
+          (Status code ${axiosError.response?.status}). For more information see dev console.`,
           true,
         )
       } else if (
-        errorEvent.reason.response.data !== undefined &&
-        typeof errorEvent.reason.response.data.errorMessage === 'string'
+        axiosError.response?.data !== undefined &&
+        typeof axiosError.response?.data?.errorMessage === 'string'
       ) {
         Notifier.showErrorMessage(
-          `Network request failed with "${errorEvent.reason.response.data.errorMessage}"
-          (Status code ${errorEvent.reason.response.status}). For more information see dev console.`,
+          `Network request failed with "${axiosError.response.data?.errorMessage}"
+          (Status code ${axiosError.response.status}). For more information see dev console.`,
           true,
         )
       } else {
         Notifier.showErrorMessage(
-          `Network request failed with "${errorEvent.reason.message}". For more information see dev console.`,
+          `Network request failed with "${axiosError.message}". For more information see dev console.`,
           true,
         )
       }
@@ -45,9 +55,20 @@ export default boot((): void => {
     }
 
     Notifier.showErrorMessage(
-      `Unexpected error: "${errorEvent}". For more information see dev console.`,
+      `Unexpected error: "${generalError}". For more information see dev console.`,
       true,
     )
     return true
+  }
+
+  window.onunhandledrejection = function (errorEvent) {
+    appErrorHandler(errorEvent.reason as Error)
+  }
+
+  // registering this error handler is specifically important as Vue seems to catch errors (and does not propagate them)
+  // if thrown inside a try block, even if there is no catch and converts them implicitly to an app error.
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  app.config.errorHandler = function (err, instance, info) {
+    appErrorHandler(err as Error)
   }
 })
