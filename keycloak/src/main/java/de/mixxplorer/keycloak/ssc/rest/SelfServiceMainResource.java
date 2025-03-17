ace83cecaa56b9efa74bbfbb88661415bda78c3d
Johannes Reppin <johannes.reppin@desy.de>
2025-03-17 08:06:17 +0100

    switch to new CORS SPI

--- keycloak/src/main/java/de/mixxplorer/keycloak/ssc/rest/SelfServiceMainResource.java
+++ keycloak/src/main/java/de/mixxplorer/keycloak/ssc/rest/SelfServiceMainResource.java
@@ -4,9 +4,12 @@ import jakarta.ws.rs.ForbiddenException;
 import jakarta.ws.rs.HttpMethod;
 import jakarta.ws.rs.NotAuthorizedException;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
 import org.keycloak.models.ClientModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.services.cors.Cors;
+import org.keycloak.http.HttpRequest;
+import org.keycloak.http.HttpResponse;
 import org.keycloak.services.managers.AppAuthManager;
 import org.keycloak.services.managers.AuthenticationManager;
 
@@ -24,46 +27,51 @@ public class SelfServiceMainResource {
     @Path("")
     public Object getSelfServiceResources() {
         // Prepare CORS response
-        final var httpRequest = keycloakSession.getContext().getHttpRequest();
-        final var httpResponse = keycloakSession.getContext().getHttpResponse();
-        var cors = Cors.add(httpRequest).auth().allowedMethods(HttpMethod.GET, HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE).auth();
+        final HttpRequest request = keycloakSession.getContext().getHttpRequest();
+        final HttpResponse response = keycloakSession.getContext().getHttpResponse();
 
-        // Do not continue, if the SSC_CLIENT_ID client is not available as this is an invalid configuration
+        Cors cors = Cors.builder().allowedMethods(HttpMethod.GET, HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE).auth()
+                .exposedHeaders(Cors.ACCESS_CONTROL_ALLOW_METHODS);
+
+        // Do not continue, if the SSC_CLIENT_ID client is not available as this is an
+        // invalid configuration
         final var sscClient = this.keycloakSession.clients().getClientByClientId(
                 this.keycloakSession.getContext().getRealm(), SSC_CLIENT_ID);
         if (sscClient == null) {
-            cors.allowAllOrigins().build(httpResponse);
+            cors.allowAllOrigins();
             throw new ForbiddenException(String.format("Self Service Clients not activated on this realm. "
                     + "Please ask your admin to create the %s client.", SSC_CLIENT_ID));
         }
 
-        // Handle all CORS OPTIONS requests. As we are having the same CORS options for all routes, we return always
-        // a valid CORS response, even if the route does not exist. This might be a bit misleading, but keeps
-        // the code tidy. Furthermore, Keycloak is internally doing the same with its Admin API.
+        // Handle all CORS OPTIONS requests. As we are having the same CORS options for
+        // all routes, we return always a valid CORS response, even if the route does
+        // not exist. This might be a bit misleading, but keeps the code tidy.
+        // Furthermore, Keycloak is internally doing the same with its Admin API.
         // see also org.keycloak.services.resources.admin
-        if (httpRequest.getHttpMethod().equalsIgnoreCase(HttpMethod.OPTIONS)) {
-            return new SelfServicePreflight(cors);
+        if (request.getHttpMethod().equalsIgnoreCase(HttpMethod.OPTIONS)) {
+            return new SelfServicePreflight().answerCors();
         }
 
-        // Check whether the user is authenticated. Doing this here ensures all requests forwarded to our real resources
-        // are authenticated.
+        // Check whether the user is authenticated. Doing this here ensures all requests
+        // forwarded to our real resources are authenticated.
         AuthenticationManager.AuthResult authResult;
         try {
             authResult = checkPermissionsAndGetUser(sscClient);
         } catch (Exception exc) {
-            cors.allowAllOrigins().build(httpResponse);
+            cors.allowAllOrigins();
             throw exc;
         }
 
-        // Add cors options to prepared response, which will be used (implicitly apparently) when building responses
-        // downstream.
-        cors.allowedOrigins(authResult.getToken()).build(httpResponse);
+        // Add cors options to prepared response, which will be used (implicitly
+        // apparently) when building responses downstream.
+        cors.allowedOrigins(authResult.getToken()).add(Response.ok(response));
 
         return new SelfServiceResources(this.keycloakSession, authResult);
     }
 
     // Authenticate users centrally for all routes of the ssc API.
-    // Users must be in the SSC_MANAGER_CLIENT_ROLE client role of client SSC_CLIENT_ID
+    // Users must be in the SSC_MANAGER_CLIENT_ROLE client role of client
+    // SSC_CLIENT_ID
     private AuthenticationManager.AuthResult checkPermissionsAndGetUser(ClientModel sscClient) {
         final var auth = new AppAuthManager.BearerTokenAuthenticator(this.keycloakSession);
         final var authResult = auth.authenticate();
@@ -71,7 +79,8 @@ public class SelfServiceMainResource {
             throw new NotAuthorizedException("Not authorized");
         } else if (authResult.getToken().getRealmAccess() == null
                 || authResult.getToken().getResourceAccess(sscClient.getClientId()) == null
-                || !authResult.getToken().getResourceAccess(sscClient.getClientId()).isUserInRole(SSC_MANAGER_CLIENT_ROLE)) {
+                || !authResult.getToken().getResourceAccess(sscClient.getClientId())
+                        .isUserInRole(SSC_MANAGER_CLIENT_ROLE)) {
             throw new ForbiddenException(String.format("Token is missing the %s client role of client %s.",
                     SSC_MANAGER_CLIENT_ROLE, SSC_CLIENT_ID));
         }
--- keycloak/src/main/java/de/mixxplorer/keycloak/ssc/rest/SelfServicePreflight.java
+++ keycloak/src/main/java/de/mixxplorer/keycloak/ssc/rest/SelfServicePreflight.java
@@ -1,21 +1,27 @@
 package de.mixxplorer.keycloak.ssc.rest;
 
+import org.keycloak.services.cors.Cors;
+
 import jakarta.ws.rs.OPTIONS;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
-import org.keycloak.services.cors.Cors;
 
 // Return CORS response for all sub paths.
 public class SelfServicePreflight {
-    Cors cors;
 
-    public SelfServicePreflight(Cors cors) {
-        this.cors = cors;
-    }
+    /**
+     CORS Preflight request.
 
+     @return Response
+     */
     @OPTIONS
     @Path("/{any:.*}")
     public Response answerCors() {
-        return cors.builder(Response.ok()).preflight().build();
+        return Cors.builder()
+            .preflight()
+            .allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS")
+            .exposedHeaders(Cors.DEFAULT_ALLOW_HEADERS)
+            .auth()
+            .add(Response.ok());
     }
 }
