f1b89552d95b193db57c51ab63fdeaae38c2b372
Felix Gohla <felix@mixxplorer.de>
2025-10-22 10:45:55 +0200

    fix(frontend): fix linting

--- frontend/src/utils/notifier.ts
+++ frontend/src/utils/notifier.ts
@@ -1,7 +1,7 @@
 import { Notify, QNotifyUpdateOptions } from 'quasar'
 export class Notifier {
   // Chose return type Function because Quasar is returning it as well
-  public static showSuccessMessage(message: string, timeout = 2500): (props?: QNotifyUpdateOptions | undefined) => void {
+  public static showSuccessMessage(message: string, timeout = 2500): (props?: QNotifyUpdateOptions) => void {
     return Notify.create({
       type: 'positive',
       message,
@@ -17,7 +17,7 @@ export class Notifier {
     })
   }
 
-  public static showWarningMessage(message: string): (props?: QNotifyUpdateOptions | undefined) => void {
+  public static showWarningMessage(message: string): (props?: QNotifyUpdateOptions) => void {
     return Notify.create({
       type: 'warning',
       message,
@@ -27,7 +27,7 @@ export class Notifier {
   public static showErrorMessage(
     message: string,
     closeBtn?: string | boolean,
-  ): (props?: QNotifyUpdateOptions | undefined) => void {
+  ): (props?: QNotifyUpdateOptions) => void {
     const actionList = []
     if (closeBtn === undefined || closeBtn === true) {
       actionList.push({
@@ -50,7 +50,7 @@ export class Notifier {
     })
   }
 
-  public static showDefaultSaveSuccessMessage(): (props?: QNotifyUpdateOptions | undefined) => void {
+  public static showDefaultSaveSuccessMessage(): (props?: QNotifyUpdateOptions) => void {
     return this.showSuccessMessage('Changes successfully saved!')
   }
 }
